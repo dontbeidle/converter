c4f11bd0e81debb28895f933a733513b4670f279
Atabek <superatabekt@gmail.com>
2025-04-28 16:40:41 +0500

    Enhance converter functionality to support number-to-word conversion and improve text field management

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { latinToCyrillic, cyrillicToLatin } from "kaalin";
+import { latinToCyrillic, cyrillicToLatin, toWord } from "kaalin";
 
 const converterSelectOption = document.getElementById(
   "converter-select-option"
 );
+
 const textFieldOne = document.getElementById("text-field-one");
 const textFieldTwo = document.getElementById("text-field-two");
 const latinRadio = document.getElementById("latin");
@@ -12,15 +13,38 @@ latinRadio.checked = true;
 
 const converText = () => {
   const text = textFieldOne.value;
-
-  if (latinRadio.checked) {
-    textFieldTwo.value = cyrillicToLatin(text);
-  } else if (cyrillicRadio.checked) {
-    textFieldTwo.value = latinToCyrillic(text);
+  if (converterSelectOption.value === "text") {
+    clearTextFields();
+    if (latinRadio.checked) {
+      textFieldTwo.value = cyrillicToLatin(text);
+    } else if (cyrillicRadio.checked) {
+      textFieldTwo.value = latinToCyrillic(text);
+    }
+  } else if (converterSelectOption.value === "numbers") {
+    clearTextFields();
+    if (/^\d+$/.test(text) && text !== "") {
+      if (latinRadio.checked) {
+        textFieldTwo.value = toWord(text);
+      } else if (cyrillicRadio.checked) {
+        textFieldTwo.value = latinToCyrillic(toWord(text));
+      }
+    }
   }
+};
+
+const clearTextFields = () => {
+  textFieldTwo.value = "";
 };
 
 textFieldOne.addEventListener("input", converText);
 textFieldTwo.addEventListener("input", converText);
-cyrillicRadio.addEventListener("change", converText);
 latinRadio.addEventListener("change", converText);
+cyrillicRadio.addEventListener("change", converText);
+
+// else if (converterSelectOption.value === "numbers") {
+//     if (latinRadio.checked) {
+//       textFieldTwo.value = cyrillicToLatin(toWord(text));
+//     } else if (cyrillicRadio.checked) {
+//       textFieldTwo.value = latinToCyrillic(toWord(text));
+//     }
+//   }
